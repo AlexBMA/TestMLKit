f093424ac553d24fdf7b7ae55bf593a4733a307a
AlexBMA <burghelea.alex@gmail.com>
2018-11-28 17:02:05 +0200

    .... more updates 2

--- app/src/main/java/com/example/user/testmlkit/ImagesActivity.java
+++ app/src/main/java/com/example/user/testmlkit/ImagesActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.testmlkit;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,9 +21,11 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.ml.common.FirebaseMLException;
 import com.google.firebase.ml.custom.FirebaseModelDataType;
 import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
 import com.google.firebase.ml.custom.FirebaseModelInterpreter;
 import com.google.firebase.ml.custom.FirebaseModelManager;
 import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
 import com.google.firebase.ml.custom.model.FirebaseCloudModelSource;
 import com.google.firebase.ml.custom.model.FirebaseModelDownloadConditions;
 
@@ -31,12 +37,8 @@ public class ImagesActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private ImageAdapter mAdapter;
 
-    private DatabaseReference  mDatabaseRef;
+    private DatabaseReference mDatabaseRef;
     private List<Upload> mUploads;
-
-
-
-
 
 
     private void getCloudModel() throws FirebaseMLException {
@@ -63,12 +65,35 @@ public class ImagesActivity extends AppCompatActivity {
 
         FirebaseModelManager.getInstance().registerCloudModelSource(cloudSource);
 
-        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
-                .setCloudModelName("fruit_detector")
-                //.setLocalModelName("my_local_model")
-                .build();
-        FirebaseModelInterpreter firebaseInterpreter =
-                FirebaseModelInterpreter.getInstance(options);
+
+        Log.e("%%%", "msg ");
+
+    }
+
+    private Bitmap getYourInputImage() {
+        // This method is just for show
+        return Bitmap.createBitmap(0, 0, Bitmap.Config.ALPHA_8);
+    }
+
+    private float[][][][] bitmapToInputArray() {
+
+        Bitmap bitmap = getYourInputImage();
+        int batchNum = 0;
+        float[][][][] input = new float[1][32][32][3];
+        for (int x = 0; x < 32; x++) {
+            for (int y = 0; y < 32; y++) {
+                int pixel = bitmap.getPixel(x, y);
+                input[batchNum][x][y][0] = Color.red(pixel) / 255.0f;
+                input[batchNum][x][y][1] = Color.green(pixel) / 255.0f;
+                input[batchNum][x][y][2] = Color.blue(pixel) / 255.0f;
+            }
+        }
+
+
+        return input;
+    }
+
+    private FirebaseModelInputOutputOptions createInputOutputOptions() throws FirebaseMLException {
 
         FirebaseModelInputOutputOptions inputOutputOptions =
                 new FirebaseModelInputOutputOptions.Builder()
@@ -76,10 +101,52 @@ public class ImagesActivity extends AppCompatActivity {
                         .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 3})
                         .build();
 
-        Log.e("%%%","msg ");
-
+        return inputOutputOptions;
     }
 
+    private FirebaseModelInterpreter createInterpreter() throws FirebaseMLException {
+        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
+                .setCloudModelName("fruit_detector")
+                //.setLocalModelName("my_local_model")
+                .build();
+        FirebaseModelInterpreter firebaseInterpreter = FirebaseModelInterpreter.getInstance(options);
+
+        return firebaseInterpreter;
+    }
+
+
+    private void runInference() throws FirebaseMLException {
+        FirebaseModelInterpreter firebaseInterpreter = createInterpreter();
+        float[][][][] input = bitmapToInputArray();
+        FirebaseModelInputOutputOptions inputOutputOptions = createInputOutputOptions();
+
+
+        // [START mlkit_run_inference]
+        FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
+                .add(input)  // add() as many input arrays as your model requires
+                .build();
+
+        firebaseInterpreter.run(inputs, inputOutputOptions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseModelOutputs>() {
+                            @Override
+                            public void onSuccess(FirebaseModelOutputs result) {
+                                float[][] output = result.getOutput(0);
+                                float[] probabilities = output[0];
+                            }
+                        }
+                ).addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                            }
+                        }
+                );
+
+        // [END mlkit_run_inference]
+
+    }
 
 
     @Override
@@ -103,31 +170,31 @@ public class ImagesActivity extends AppCompatActivity {
 
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
 
-        Log.e("#######################","made it her");
+        Log.e("#######################", "made it her");
 
         mDatabaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
 
 
-                    Log.e("####_$$$$$",postSnapshot.child("mImageUrl").getValue().toString()+"");
+                    Log.e("####_$$$$$", postSnapshot.child("mImageUrl").getValue().toString() + "");
 
                     Upload upload = new Upload(postSnapshot.child("name").getValue().toString(),
-                            postSnapshot.child("mImageUrl").getValue().toString()+"");
-                            //postSnapshot.getValue(Upload.class);
+                            postSnapshot.child("mImageUrl").getValue().toString() + "");
+                    //postSnapshot.getValue(Upload.class);
 
                     mUploads.add(upload);
                 }
 
-                mAdapter = new ImageAdapter(ImagesActivity.this,mUploads);
+                mAdapter = new ImageAdapter(ImagesActivity.this, mUploads);
 
                 mRecyclerView.setAdapter(mAdapter);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(ImagesActivity.this,databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(ImagesActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
 
             }
         });
